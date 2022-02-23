2d156cd8ec4afbb66aff35c794b00a79e7d931fe
Rake <bencehetes@gmail.com>
2022-02-23 16:27:07 +0100

    done dude

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -1,72 +1,34 @@
-/*
-g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
-*/
 #include "Simple_window.h"
 #include "Graph.h"
+using namespace Graph_lib;
 
-double one(double) { return 1; }
+int main(){
+    Simple_window win { Point{100,100},600,400,"My Window" };
+    Image img {Point{100,100}, "handsome.jpg"};
 
-double square(double x) { return x*x; }
+    Text title {Point{50,50}, "Love at first sight <3"};
+	title.set_font(NewFont::times_bold);
+	title.set_font_size(20);
 
-int main()
-{
-    using namespace Graph_lib;
+    Axis x {Axis::x, Point{200,300}, 200, 100/20, "x axis"};
+    Axis y {Axis::y, Point{200,300}, 200, 100/20, "y axis"};
 
-    int xmax = 1280;
-    int ymax = 720;
+    Function cosfunc {cos,0,100,Point{200,200},1000,50,50};
 
-    int x_orig = xmax/2;
-    int y_orig = ymax/2;
+    Polygon pol;
+        pol.add(Point{60,130});
+        pol.add(Point{30,150});
+        pol.add(Point{80,220});
+        pol.add(Point{130,150});
+        pol.add(Point{100,130});
+        pol.add(Point{80,150});
+        pol.set_fill_color(Color::red);
 
-    int rmin = -11;
-    int rmax = 11;
-    
-    int n_points = 400;
-
-    Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
-
-	Point origo {x_orig, y_orig};
-
-	int xlength = xmax - 40;
-	int ylength = ymax - 40;
-
-	int xscale = 30, yscale = 30;
-
-	Function s (one, rmin, rmax, origo, n_points, xscale, yscale);
-	Function sq (square, rmin, rmax, origo, n_points, xscale, yscale);
-	Function cos_func ( [] (double x) { return cos(x); },
-						rmin, rmax, origo, n_points, xscale, yscale);
-
-	Axis x {Axis::x, Point{20, y_orig}, xlength, xlength/xscale, "x"};
-	Axis y {Axis::y, Point{x_orig, ylength + 20}, ylength, ylength/yscale, "y"};
-
-	Rectangle r {Point{200,200}, 100, 50};
-
-	r.set_fill_color(Color::yellow);
-	r.set_style(Line_style(Line_style::dash, 4));
-
-	Text t {Point{200,400}, "Hello graphics!"};
-	t.set_font(NewFont::times_bold);
-	t.set_font_size(20);
-
-	Image ii {Point{100,100}, "badge.jpg"};
-
-	Circle c {Point{700,700}, 100};
-
-	Ellipse e {Point{500,500}, 100, 50};
-	e.set_fill_color(Color::red);
-
-	win.attach(e);
-	win.attach(ii);
-	win.attach(c);
-	win.attach(t);
-	win.attach(r);
-	win.attach(s);
-	win.attach(sq);
-	win.attach(cos_func);
-	win.attach(x);
-	win.attach(y);
-
+    win.attach(img);
+    win.attach(pol);
+    win.attach(title);
+    win.attach(x);
+    win.attach(y);
+    win.attach(cosfunc);
     win.wait_for_button();
-
 }
